06c098b49432ea3ba1850b11b2db38e593c6a6a0
yashchovatiya <66351236+yashchovatiya@users.noreply.github.com>
2021-04-19 16:46:36 +0530

    final website

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,7 @@ import {persistStore} from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
-const middlewares=[];
-if(process.env.NODE_ENV==="development"){
-    middlewares.push(logger);
-}
-
+const middlewares=[logger];
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
